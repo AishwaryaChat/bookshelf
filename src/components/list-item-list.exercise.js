274e12c35942122468455de0a00392db54239ca8
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-05-02 19:23:02 +0530

    Add caching

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -2,7 +2,9 @@
 import {jsx} from '@emotion/core'
 
 // 🐨 you'll need useQuery from 'react-query'
+import {useQuery} from 'react-query'
 // 🐨 and client from 'utils/api-client'
+import {client} from 'utils/api-client.exercise'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
 
@@ -12,12 +14,14 @@ function ListItemList({
   noListItems,
   noFilteredListItems,
 }) {
+  const {data: listItems} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () =>
+      client('list-items', {token: user.token}).then(data => data.listItems),
+  })
   // 🐨 call useQuery to get the list-items from the 'list-items' endpoint
   // queryKey should be 'list-items'
   // queryFn should call the 'list-items' endpoint
-
-  // 🐨 assign this to the list items you get back from react-query
-  const listItems = null
 
   const filteredListItems = listItems?.filter(filterListItems)
 
--- src/components/status-buttons.exercise.js
+++ src/components/status-buttons.exercise.js
@@ -12,9 +12,11 @@ import {
 import Tooltip from '@reach/tooltip'
 // 🐨 you'll need useQuery, useMutation, and queryCache from 'react-query'
 // 🐨 you'll also need client from 'utils/api-client'
+import { useQuery, useMutation, queryCache } from 'react-query'
 import {useAsync} from 'utils/hooks'
 import * as colors from 'styles/colors'
 import {CircleButton, Spinner} from './lib'
+import { client } from 'utils/api-client.exercise'
 
 function TooltipButton({label, highlight, onClick, icon, ...rest}) {
   const {isLoading, isError, error, run} = useAsync()
@@ -54,8 +56,11 @@ function StatusButtons({user, book}) {
 
   // 🐨 search through the listItems you got from react-query and find the
   // one with the right bookId.
-  const listItem = null
-
+  const {data: listItems} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () => client('list-items', {token: user.token}).then(data => data.listItems)
+  })
+  const listItem = listItems?.find(li => li.bookId === book.id) ?? null
   // 💰 for all the mutations below, if you want to get the list-items cache
   // updated after this query finishes then use the `onSettled` config option
   // to queryCache.invalidateQueries('list-items')
@@ -64,14 +69,34 @@ function StatusButtons({user, book}) {
   // the mutate function should call the list-items/:listItemId endpoint with a PUT
   //   and the updates as data. The mutate function will be called with the updates
   //   you can pass as data.
+  const [update] = useMutation(updates => client(`list-items/:${updates.id}`, {
+    token: user.token,
+    method: "PUT",
+    data: updates
+  }), {
+    onSettled: () => queryCache.invalidateQueries('list-items')
+  })
 
   // 🐨 call useMutation here and assign the mutate function to "remove"
   // the mutate function should call the list-items/:listItemId endpoint with a DELETE
+  const [remove] = useMutation(({id}) => client(`list-items/${id}`, {
+    token: user.token,
+    method: "DELETE",
+  }), {
+    onSettled: () => queryCache.invalidateQueries('list-items')
+  })
 
   // 🐨 call useMutation here and assign the mutate function to "create"
   // the mutate function should call the list-items endpoint with a POST
   // and the bookId the listItem is being created for.
-
+  const [create] = useMutation(({bookId}) => client(`list-items`, {
+    token: user.token,
+    method: "POST",
+    data: {bookId}
+  }), {
+    onSettled: () => queryCache.invalidateQueries('list-items')
+  })
+  console.log("listItem inside exxercise", listItem)
   return (
     <React.Fragment>
       {listItem ? (
@@ -81,6 +106,7 @@ function StatusButtons({user, book}) {
             highlight={colors.yellow}
             // 🐨 add an onClick here that calls update with the data we want to update
             // 💰 to mark a list item as unread, set the finishDate to null
+            onClick={()=> update({id: listItem.id, finishDate: null})}
             // {id: listItem.id, finishDate: null}
             icon={<FaBook />}
           />
@@ -90,6 +116,7 @@ function StatusButtons({user, book}) {
             highlight={colors.green}
             // 🐨 add an onClick here that calls update with the data we want to update
             // 💰 to mark a list item as read, set the finishDate
+            onClick={() => update({id: listItem.id, finishDate: Date.now()})}
             // {id: listItem.id, finishDate: Date.now()}
             icon={<FaCheckCircle />}
           />
@@ -99,6 +126,7 @@ function StatusButtons({user, book}) {
         <TooltipButton
           label="Remove from list"
           highlight={colors.danger}
+          onClick={() => remove({id: listItem.id})}
           // 🐨 add an onClick here that calls remove
           icon={<FaMinusCircle />}
         />
@@ -106,6 +134,7 @@ function StatusButtons({user, book}) {
         <TooltipButton
           label="Add to list"
           highlight={colors.indigo}
+          onClick={() => create({bookId: book.id})}
           // 🐨 add an onClick here that calls create
           icon={<FaPlusCircle />}
         />
