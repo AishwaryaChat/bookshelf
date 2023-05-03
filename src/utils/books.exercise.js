635a44ea82dfb04712d5a949170c3b32216ae81b
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-05-03 14:32:28 +0530

    Add useBookSearch hook

--- src/utils/books.exercise.js
+++ src/utils/books.exercise.js
@@ -1,20 +1,37 @@
-import { useQuery } from "react-query"
-import { client } from "./api-client.exercise"
+import {useQuery} from 'react-query'
+import {client} from './api-client.exercise'
 import bookPlaceholderSvg from 'assets/book-placeholder.svg'
 
 const loadingBook = {
-    title: 'Loading...',
-    author: 'loading...',
-    coverImageUrl: bookPlaceholderSvg,
-    publisher: 'Loading Publishing',
-    synopsis: 'Loading...',
-    loadingBook: true,
-  }
+  title: 'Loading...',
+  author: 'loading...',
+  coverImageUrl: bookPlaceholderSvg,
+  publisher: 'Loading Publishing',
+  synopsis: 'Loading...',
+  loadingBook: true,
+}
+
+const loadingBooks = Array.from({length: 10}, (v, index) => ({
+  id: `loading-book-${index}`,
+  ...loadingBook,
+}))
 
 export const useBook = ({bookId, user}) => {
-    const {data} = useQuery({
-        queryKey: ['book', {bookId}],
-        queryFn: () => client(`books/${bookId}`, {token: user.token}).then(data => data.book)
-    })
-    return data ?? loadingBook
+  const {data} = useQuery({
+    queryKey: ['book', {bookId}],
+    queryFn: () =>
+      client(`books/${bookId}`, {token: user.token}).then(data => data.book),
+  })
+  return data ?? loadingBook
+}
+
+export const useBookSearch = ({query, user}) => {
+  const {data: books = loadingBooks, ...rest} = useQuery({
+    queryKey: ['bookSearch', {query}],
+    queryFn: () =>
+      client(`books?query=${encodeURIComponent(query)}`, {
+        token: user.token,
+      }).then(data => data.books),
+  })
+  return {books, ...rest}
 }
