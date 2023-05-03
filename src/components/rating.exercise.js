acaa4a1cc4f1ef8d93ef401505e4c449d2c222d7
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-05-03 15:52:25 +0530

    Add failure for ratings on book page

--- src/components/rating.exercise.js
+++ src/components/rating.exercise.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {FaStar} from 'react-icons/fa'
 import * as colors from 'styles/colors'
 import {useUpdateListItem} from 'utils/list-items.exercise'
+import {ErrorMessage} from './lib'
 const visuallyHiddenCSS = {
   border: '0',
   clip: 'rect(0 0 0 0)',
@@ -18,7 +19,7 @@ const visuallyHiddenCSS = {
 
 function Rating({listItem, user}) {
   const [isTabbing, setIsTabbing] = React.useState(false)
-  const [update] = useUpdateListItem({user})
+  const [update, {isError, error}] = useUpdateListItem({user})
 
   React.useEffect(() => {
     function handleKeyDown(event) {
@@ -98,6 +99,13 @@ function Rating({listItem, user}) {
       }}
     >
       <span css={{display: 'flex'}}>{stars}</span>
+      {isError && (
+        <ErrorMessage
+          error={error}
+          variant="inline"
+          css={{marginLeft: 6, fontSize: '0.7em'}}
+        />
+      )}
     </div>
   )
 }
