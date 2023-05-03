5cf46a55a6487796bc431bc991e6c80c69b38326
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2023-05-03 15:41:50 +0530

    Add QueryCacheConfig

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,12 +3,29 @@ import './bootstrap'
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const queryConfigs = {
+  queries: {
+    refetchOnWindowFocus: false,
+    useErrorBoundary: true,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      else if (failureCount < 2) return true
+      else return false
+    },
+  },
+}
 
 // ignore the rootRef in this file. I'm just doing it here to make
 // the tests I write to check your work easier.
 export const rootRef = {}
 loadDevTools(() => {
   const root = createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(
+    <ReactQueryConfigProvider config={queryConfigs}>
+      <App />
+    </ReactQueryConfigProvider>,
+  )
   rootRef.current = root
 })
